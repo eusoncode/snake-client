ad522be30daaea952e7dd05806d59791bff06b7e
eusoncode <git@github.com:eusoncode/Kata.git>
2023-07-15 02:14:12 -0600

    added the WASD Movement

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -8,8 +11,17 @@ const setupInput = function () {
 };
 
 const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    console.log('Game terminated');
+  // bind movement commands to W A S D keys and send commands to game server
+  if (key === "w") {
+    connection.write("Move: up");
+  } else if (key === "a") {
+    connection.write("Move: left");
+  } else if (key === "s") {
+    connection.write("Move: down");
+  } else if (key === "d") {
+    connection.write("Move: right");
+  } else if (key === "\u0003") {// terminate the game with ctrl+c
+    console.log("Game terminated!");
     process.exit();
   }
 };
