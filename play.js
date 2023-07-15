ab53774e8ccb234c915701cbd10e371bbab8b784
eusoncode <git@github.com:eusoncode/Kata.git>
2023-07-14 23:24:23 -0600

    import client file

--- play.js
+++ play.js
@@ -1,20 +1,3 @@
-const net = require("net");
+const { connect } = require("./client");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost', // IP address here,
-    port: 50541,// PORT number here,
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("connect", () => {
-    // code that does something when the connection is first established
-    console.log("Connected successfully");
-  });
-
-  return conn;
-};
-connect();
