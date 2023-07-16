0920f7d22d3c75648d0a6c30fdad5e1fc10c64c6
eusoncode <git@github.com:eusoncode/Kata.git>
2023-07-15 23:45:05 -0600

    Updated the object key-value for the WASD movement

--- constants.js
+++ constants.js
@@ -1,7 +1,18 @@
+// Define the useful input data and export them
 const IP = 'localhost';
 const PORT = 50541;
+const playerName = process.argv.slice(2);
+const cmdLineObject = {
+  "w": "Move: up",
+  "a": "Move: left",
+  "s": "Move: down",
+  "d": "Move: right",
+  "t": 'Say: Hey Player2'
+};
 
 module.exports = {
   IP,
   PORT,
+  playerName,
+  cmdLineObject
 };
