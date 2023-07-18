fae50dda251e54118404b471ea159357e11ad382
eusoncode <git@github.com:eusoncode/Kata.git>
2023-07-17 21:45:41 -0600

    Removed horizontal line spacing and check code with eslint

--- play.js
+++ play.js
@@ -2,7 +2,5 @@
 const { connect } = require("./client");
 const { setupInput } = require("./input");
 
-console.log('Connecting ...');
-
 // Call setupInput function with connection function passed into it.
 setupInput(connect());
